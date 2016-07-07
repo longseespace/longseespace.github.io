415d48f76a53b4b8b04961a8767c2a62a95607b4
Long Nguyen <long@nomadreact.com>
2016-07-08 00:18:48 +0700

    New secure token

--- deploy.sh
+++ deploy.sh
@@ -6,9 +6,6 @@ set -e # exit with nonzero exit code if anything fails
 cd public
 git init
 
-# add CNAME record
-# echo "phimhd.pw" > CNAME
-
 # inside this git repo we'll pretend to be a new user
 git config user.name "Travis CI"
 git config user.email "travis@nomadreact.com"
@@ -22,4 +19,4 @@ git commit -m "Deploy to GitHub Pages"
 # repo's gh-pages branch. (All previous history on the gh-pages branch
 # will be lost, since we are overwriting it.) We redirect any output to
 # /dev/null to hide any sensitive credential data that might otherwise be exposed.
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:master > /dev/null 2>&1
